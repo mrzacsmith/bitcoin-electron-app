ed875a075cb1dedfed9bd8269805f186479deb20
Zac Smith <zrs3141592@gmail.com>
2020-01-28 10:13:08 -0700

    create basic axios call
    still having issues to get display, not sure what is causing error

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ const path = require("path");
 const BrowserWindow = electron.remote.BrowserWindow;
 
 const notifyBtn = document.getElementById("notifyBtn");
+let price = document.querySelector("notifyBtn");
+let targetPrice = document.getElementById("targetPrice");
+
+const getBTC = () => {
+  axios
+    .get(
+      "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD"
+    )
+    .then(res => {
+      const cryptos = res.data.BTC.USD;
+      price.innerHTML = `$${cryptos.toLocaleString("en")}`;
+    })
+    .catch(error => {
+      console.log("error", error);
+    });
+};
+
+getBTC();
+setInterval(getBTC, 30000);
 
 notifyBtn.addEventListener("click", function(event) {
   // const modalPath = path.join("./", __dirname, "add.html");
