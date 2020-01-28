e41392a4cfbf3a1b5f389cbe27cb410b5b8f85ca
Zac Smith <zrs3141592@gmail.com>
2020-01-28 09:50:09 -0700

    create modal that is transparent

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ const BrowserWindow = electron.remote.BrowserWindow;
 const notifyBtn = document.getElementById("notifyBtn");
 
 notifyBtn.addEventListener("click", function(event) {
-  const modalPath = path.join("file://", __dirname, "add.html");
-  let win = new BrowserWindow({ width: 400, height: 200 });
+  // const modalPath = path.join("./", __dirname, "add.html");
+  const modalPath = `file:///home/silverback/Documents/Projects/electron/bitcoin-app/src/add.html`;
+  let win = new BrowserWindow({
+    frame: false,
+    transparent: true,
+    width: 400,
+    height: 200
+  });
   win.on("close", function() {
     win = null;
   });
